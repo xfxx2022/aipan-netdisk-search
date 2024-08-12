40fddcdedc63bf0f577c5d2c96984cd4eab09522
薰风习习 <25678988+xfxx2022@users.noreply.github.com>
2024-08-12 17:59:48 +0800

    更新 index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -107,9 +107,7 @@ onMounted(async () => {
     </div>
   </div>
 </template>
-<script>
   window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
-</script>
 <script defer src="/_vercel/insights/script.js"></script>
 
 <style scoped>
