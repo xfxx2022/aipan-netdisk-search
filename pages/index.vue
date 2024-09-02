3d42d49f1355b0d294ee2391e30bccb96b135975
薰风习习 <25678988+xfxx2022@users.noreply.github.com>
2024-09-02 17:59:48 +0800

    更新 index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -99,7 +99,8 @@ onMounted(async () => {
       </div>
     </div>
 
-    <div class="p-4">
+    <div class="py-2 h-25 fixed bottom-0 left-0 right-0 bg-white dark:bg-slate-800 space-y-1">
+      </div>
       <p class="text-center text-[8px] sm:text-[12px] text-slate-400">
         声明：本站内容皆来自网络公开资源。本站不储存、复制、传播任何文件，不做任何盈利，仅作个人公益学习，请勿非法&商业传播，如有侵权，请及时
         <a class="text-blue-500 underline" href="mailto:aidengrong@gmail.com" title="点击留言"> 留言 </a> 告知删除。
